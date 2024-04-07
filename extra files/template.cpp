19267e83e9086ccabb503474b61665c625f0a73e
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2024-04-07 22:48:00 +0200

    Add a folder for extra files
    Currently includes only a template for C++ LeetCoders

---
+++ extra files/template.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    Solution() { ios_base::sync_with_stdio(0); }
+    /*
+    typename function(parameterList)
+    {
+       return value;
+    }
+    */
+
+};
+
+// Comment from here to the end before submission ^_^
+#include <leetcode.h>
+using namespace leet;
+main()
+{
+    //Feel free to change file paths ^_^
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+    Solution sol = Solution();
+    //Write your code here
+
+    return 0;
+}
